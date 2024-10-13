f10b8382f33c93169d4ff88528129c25960c6f1d
Frans Ekman <104152557+fransek@users.noreply.github.com>
2024-10-13 16:25:34 +0200

    vitest

---
+++ src/lib/createStore.ts
@@ -0,0 +1,53 @@
+export type Store<TState extends object, TActions extends object> = {
+    get: () => TState;
+    set: (setter: Setter<TState>) => TState;
+    reset: () => TState;
+    subscribe: (listener: () => void) => () => void;
+    actions: TActions;
+};
+
+export type Setter<TState extends object> =
+    | Partial<TState>
+    | ((state: TState) => Partial<TState>);
+
+export const createStore = <
+    TState extends object,
+    TActions extends object = Record<never, never>,
+>(
+    initialState: TState,
+    createActions?: (
+        set: (setter: Setter<TState>) => TState,
+        get: () => TState,
+    ) => TActions,
+): Store<TState, TActions> => {
+    let state = initialState;
+    let listeners: (() => void)[] = [];
+
+    const dispatch = () => listeners.forEach((listener) => listener());
+
+    const get = () => state;
+
+    const set = (setter: Setter<TState>) => {
+        const newState = typeof setter === "function" ? setter(state) : setter;
+        state = { ...state, ...newState };
+        dispatch();
+        return state;
+    };
+
+    const reset = () => {
+        state = initialState;
+        dispatch();
+        return state;
+    };
+
+    const subscribe = (listener: () => void) => {
+        listeners.push(listener);
+        return () => {
+            listeners = listeners.filter((l) => l !== listener);
+        };
+    };
+
+    const actions = createActions ? createActions(set, get) : ({} as TActions);
+
+    return { get, set, reset, subscribe, actions };
+};
---
+++ src/lib/useStore.ts
@@ -0,0 +1,13 @@
+import { useSyncExternalStore } from "react";
+import { Store } from "./createStore";
+
+export const useStore = <TState extends object, TActions extends object>({
+    get,
+    set,
+    reset,
+    subscribe,
+    actions,
+}: Store<TState, TActions>) => {
+    const state = useSyncExternalStore(subscribe, get);
+    return { state, actions, set, reset };
+};
