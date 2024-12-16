915155752d5c59fc0ecb5f749f294ea5dbb647fa
Frans Ekman <fransedvinekman@gmail.com>
2024-12-16 19:06:25 +0100

    ssr fix

--- global.d.ts
+++
@@ -1,7 +0,0 @@
-declare global {
-  interface Window {
-    __store_listeners?: Set<string>;
-  }
-}
-
-export {};
--- src/lib/createPersistentStore.test.ts
+++ src/lib/createPersistentStore.test.ts
@@ -6,6 +6,8 @@ describe("createPersistentStore", () => {
   const key = "test";
   const initialState = { count: 0 };
   const listener = vi.fn();
+  const initKey = `init_${key}`;
+  const storeKey = `store_${key}`;
 
   beforeEach(() => {
     localStorage.clear();
@@ -21,27 +23,22 @@ describe("createPersistentStore", () => {
     const store = createPersistentStore(key, initialState);
     store.subscribe(listener);
     store.set({ count: 1 });
-    const storedState = localStorage.getItem(`store_${key}`);
+    const storedState = localStorage.getItem(storeKey);
     expect(storedState).toBe(superjson.stringify({ count: 1 }));
   });
 
   it("should load state from localStorage if available", () => {
-    localStorage.setItem(
-      `store_init_${key}`,
-      superjson.stringify({ count: 0 }),
-    );
-    localStorage.setItem(`store_${key}`, superjson.stringify({ count: 2 }));
+    localStorage.setItem(initKey, superjson.stringify({ count: 0 }));
+    localStorage.setItem(storeKey, superjson.stringify({ count: 2 }));
     const store = createPersistentStore(key, initialState);
+    expect(store.get()).toEqual({ count: 0 });
     store.subscribe(listener);
     expect(store.get()).toEqual({ count: 2 });
   });
 
   it("should load state from sessionStorage if available", () => {
-    sessionStorage.setItem(
-      `store_init_${key}`,
-      superjson.stringify({ count: 0 }),
-    );
-    sessionStorage.setItem(`store_${key}`, superjson.stringify({ count: 2 }));
+    sessionStorage.setItem(initKey, superjson.stringify({ count: 0 }));
+    sessionStorage.setItem(storeKey, superjson.stringify({ count: 2 }));
     const store = createPersistentStore(key, initialState, null, {
       storage: "session",
     });
@@ -53,7 +50,7 @@ describe("createPersistentStore", () => {
     const store = createPersistentStore(key, initialState);
     store.subscribe(listener);
     store.set({ count: 1 });
-    localStorage.setItem(`store_${key}`, superjson.stringify({ count: 2 }));
+    localStorage.setItem(storeKey, superjson.stringify({ count: 2 }));
     window.dispatchEvent(new Event("focus"));
     expect(store.get()).toEqual({ count: 2 });
   });
@@ -72,7 +69,7 @@ describe("createPersistentStore", () => {
     const store = createPersistentStore(key, initialState);
     store.subscribe(listener);
     store.set({ count: 1 });
-    const storedState = sessionStorage.getItem(`store_${key}`);
+    const storedState = sessionStorage.getItem(storeKey);
     expect(storedState).toBeNull();
   });
 });
